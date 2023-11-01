9b2a4b047b6cde046f1d1aec3cc3cbae9e3507e7
Ivan San <ciklop3@msn.com>
2023-11-01 21:22:51 +0200

    added Pagination component

--- src/app/components/pagination/Pagination.jsx
+++ src/app/components/pagination/Pagination.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
-  return <nav className={styles.container}>Pagination</nav>;
+  return (
+    <nav className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </nav>
+  );
 };
 
 export default Pagination;
