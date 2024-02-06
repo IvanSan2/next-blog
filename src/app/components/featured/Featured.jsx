bf825fcd7400250d7a764a370e8c6a9aeaf1d22e
Ivan San <ciklop3@msn.com>
2024-02-06 10:43:24 +0200

    fetching posts from database in featured component

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,43 +1,68 @@
-import React from "react";
+"use client";
+import React, { use } from "react";
 import styles from "./featured.module.css";
-import p1 from "../../../../public/p1.jpeg";
-import whiteImage from "../../../../public/white.jpg";
+// import p1 from "../../../../public/p1.jpeg";
+// import whiteImage from "../../../../public/white.jpg";
 import FeaturedSlider from "../featuredSlider/FeaturedSlider";
+import { useEffect, useState, useMountEffect } from "react";
 
 const Featured = () => {
-  const slides = [
-    {
-      id: 1,
-      title: "Lorem ipsum.",
-      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
-      img: p1,
-    },
-    {
-      id: 2,
-      title: "Lorem ipsum dolor.",
-      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
-      img: p1,
-    },
-    {
-      id: 3,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
-      img: p1,
-    },
-    {
-      id: 4,
-      title: "Lorem ipsum dolor sit 4.",
-      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
-      img: p1,
-    },
-    {
-      id: 5,
-      title:
-        "Lorem ipsum dolor sit 5. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
-      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
-      img: whiteImage,
-    },
-  ];
+  // const slides = [
+  //   {
+  //     id: 1,
+  //     title: "Lorem ipsum.",
+  //     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
+  //     img: p1,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Lorem ipsum dolor.",
+  //     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
+  //     img: p1,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  //     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
+  //     img: p1,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Lorem ipsum dolor sit 4.",
+  //     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
+  //     img: p1,
+  //   },
+  //   {
+  //     id: 5,
+  //     title:
+  //       "Lorem ipsum dolor sit 5. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
+  //     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quam nisi magni ea laborum inventore voluptatum laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium quisquam! Harum unde sit culpa debitis.",
+  //     img: whiteImage,
+  //   },
+  // ];
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/posts?pageNo=0&pageSize=5"
+        );
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setPosts(data.posts);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -47,7 +72,9 @@ const Featured = () => {
         </b>{" "}
         where developers share knowledge
       </h1>
-      <FeaturedSlider slides={slides} />
+      {loading ? <h1>Loading...</h1> : null}
+      {error ? <h1>Error: {error.message}</h1> : null}
+      <FeaturedSlider slides={posts} />
     </div>
   );
 };
--- src/app/components/featuredSlide/FeaturedSlide.jsx
+++ src/app/components/featuredSlide/FeaturedSlide.jsx
@@ -1,12 +1,19 @@
 import styles from "./featuredSlide.module.css";
 import Image from "next/image";
+import noImage from "../../../../public/no-image.svg";
+// import noImage from "../icons/NoImage";
 
 export default function FeaturedSlide({ slide }) {
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.imageContainer}>
-          <Image src={slide.img} alt="" fill className={styles.image} />
+          <Image
+            src={slide.img ? slide.img : noImage}
+            alt=""
+            fill
+            className={slide.img ? styles.image : { objectFit: "contain" }}
+          />
           <div className={styles.textContainer}>
             {/* max 70 symbols for title */}
             <h1 className={styles.title}>
@@ -17,7 +24,30 @@ export default function FeaturedSlide({ slide }) {
             <div className={styles.user}>
               <div className={styles.userTextContainer}>
                 <span className={styles.username}>Ivan San</span>
-                <span className={styles.date}>14.12.2023</span>
+                <span className={styles.date}>
+                  {
+                    //if the date is more than 1 day ago, show the date, else show the time, if the date is less then 5 hours ago, show "just now"
+                    new Date(slide.createdAt).getTime() <
+                    new Date().getTime() - 86400000 // 1 day in ms
+                      ? new Date(slide.createdAt).toLocaleDateString()
+                      : new Date(slide.createdAt).getTime() >
+                        new Date().getTime() - 3600000 // 1 hour in ms
+                      ? "Just now"
+                      : // show how much time ago the post was created
+                        `${
+                          Math.floor(
+                            (new Date().getTime() -
+                              new Date(slide.createdAt).getTime()) /
+                              3600000
+                          ) || 1
+                        }h and ${Math.floor(
+                          (new Date().getTime() -
+                            new Date(slide.createdAt).getTime()) /
+                            600000
+                        )}m
+                         ago`
+                  }
+                </span>
               </div>
             </div>
           </div>
